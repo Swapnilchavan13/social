2251c589da6a47f8941ab72b8b5edb869d14e4b0
swapnilchavan13 <chavanswapnil822@gmail.com>
2023-04-29 22:11:24 +0530

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app= express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.set('strictQuery', false);
+const connectDB= async()=>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB Connected :${conn.connection.host}`)
+    } catch (error) {
+        console.log("Error")
+    }
+}
