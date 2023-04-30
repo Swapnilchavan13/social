a7c88e21c51313b91aa9b246822f6b39feabbcb1
swapnilchavan13 <chavanswapnil822@gmail.com>
2023-04-30 22:55:36 +0530

    update

--- index.js
+++ index.js
@@ -14,3 +14,108 @@ const connectDB= async()=>{
         console.log("Error")
     }
 }
+
+// parse application/json
+app.use(express.json());
+
+// Add cors middleware
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send({title: 'Backend is Runnning...'});
+});
+
+
+app.get('/data', async (req,res) => {
+    const book = await Book.find();
+
+    if (book) {
+        res.json(book);
+    } else {
+        res.send("Something Went wrong.");
+    }
+});
+
+// Route to get book by id
+app.get('/data/:id', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        if (!book) {
+            return res.status(404).send('Book not found');
+        }
+        res.json(book);
+    } catch (error) {
+        console.log("Err", + error);
+        res.status(500).send('Server Error');
+    }
+});
+
+// Route to add a new book
+app.post('/data', async (req, res) => {
+    try {
+        const book = new Book({
+            title: req.body.title,
+            body: req.body.body,
+            discription:req.body.discription,
+            image:req.body.image
+        });
+        await book.save();
+        res.json(book);
+    } catch (error) {
+        console.log("Err", + error);
+        res.status(500).send('Server Error');
+    }
+});
+
+app.get('/a',(req, res) => {
+    res.send({title: 'Auth is Runnning...'});
+});
+
+app.get('/auth', async (req,res) => {
+    const auth = await Auth.find();
+
+    if (auth) {
+        res.json(auth);
+    } else {
+        res.send("Something Went wrong.");
+    }
+});
+
+app.post('/auth', async (req, res) => {
+    try {
+        const auth = new Auth({
+            hospital_id : req.body.hospital_id,
+            aadhaar_num : req.body.aadhaar_num,
+            otp : req.body.otp
+        })
+        await auth.save();
+        res.json("Otp Authentication Done");
+    } catch (error) {
+        console.log("Err", + error);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+app.post('/autha', async (req, res) => {
+  try {
+    const otp = otpGenerator.generate(6, { digits: true, alphabets: false, upperCase: false, specialChars: false });
+    console.log('OTP generated:', otp);
+    const autha = new AuthA({
+      aadhaar_num : req.body.aadhaar_num,
+      otp: otp
+    })
+    await autha.save();
+    res.json({ message: "OTP generated and saved" ,otp });
+  } catch (error) {
+    console.log("Err", + error);
+    res.status(500).send('Server Error');
+  }
+});
+   
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    });
+});
